3ff80e1b1e205694893b5a7a7364ea86333ff820
ebeshero <ebbondar@gmail.com>
2026-08-13 11:29:13 -0700

    import ine for mentimeter qr code

--- footer-template.js
+++ footer-template.js
@@ -1,8 +1,9 @@
 import logo from './assets/TEI_Logo_Horizontal.svg';
+import menti from './assets/mentimeter_qr_code_p6.png';
 
 export const footerHTML = `
   <div class="footer">
     <img width="200" src="${logo}" alt="TEI-C logo" />
-    <img src="./assets/mentimeter_qr_code_p6.png" height="100" style="float: right"/>
+    <img src="${menti}" height="100" style="float: right"/>
   </div>
 `;
