be23781b94e9626e8e80e997d92376f2c0dc55b2
Raffaele Viglianti <raffaeleviglianti@gmail.com>
2026-08-13 09:40:02 -0700

    added menti

--- footer-template.js
+++ footer-template.js
@@ -3,5 +3,6 @@ import logo from './assets/TEI_Logo_Horizontal.svg';
 export const footerHTML = `
   <div class="footer">
     <img width="200" src="${logo}" alt="TEI-C logo" />
+    <img src="./assets/mentimeter_qr_code_p6.png" height="100" style="float: right"/>
   </div>
 `;
